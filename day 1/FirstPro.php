575b6fd9b4e068cb42792c4987892dc63c55ff57
shahzad-wpbrigade <shahzad.wpbrigade@gmail.com>
2023-01-03 16:28:36 +0500

    Day 1 Tasks

---
+++ day 1/FirstPro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> First Project</title>
+</head>
+<body>
+	<h2> First Project: Task 1</h2>>
+	<?php
+          $fisrtnam="Shazhad";
+          $secondynam="Abu Bakar";
+           echo ($fisrtnam.$secondynam);
+        ?>
+</body>
+</html>
---
+++ day 1/SecondyPro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Secondy Project</title>
+</head>
+<body>
+	<?php
+      $x=4;
+      $y=2;
+      //Addition,  Division, Modulus, Exponentiation
+   echo("Addition=");
+      echo ($x+$y);
+      echo("<br>");
+      echo("Sub=");
+       echo($x-$y);
+       echo("<br>");
+       echo("Mult=");
+       echo($x*$y);
+       echo("<br>");
+       echo("Division=");
+       echo($x/$y);
+       echo("<br>");
+       echo("Modulus=");
+       echo($x%$y);
+      ?>
+</body>
+</html>
