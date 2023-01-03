71781e5db03615eb219a64be026d30c2ac7d424e
Shahzad <121797615+shahzad-wpbrigade@users.noreply.github.com>
2023-01-03 18:19:33 +0500

    Add files via upload

---
+++ inventory-task2/Day2/foreachloop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$arrayname = array('[1 2 3]', '[4 5 6]', '[7 8 9]', ); 
+
+foreach ($arrayname as $value) {
+  echo "$value <br>";
+}
+?>  
+
+</body>
+</html>
---
+++ inventory-task2/Day2/simpleArray.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Arrray</title>
+</head>
+<body> 
+	        <h3>Day 2 To Task</h3>
+    <?php
+      $arrayName = array('[1 2 3]','[4 5 6]','[7 8 9]');
+      echo "This is  Numbers Show From 1 to 3 <br>".$arrayName[0] ;
+      echo("<br>");
+      echo "This is  Numbers Show From 4 to 6 <br>".$arrayName[1] ;
+      echo("<br>");
+       echo "This is  Numbers Show From 7 to 9 <br>".$arrayName[2] ;
+    ?>
+</body>
+</html> 
