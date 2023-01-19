cb9494fd203fb0304d62f89ca418b8d9bce6f792
Shahzad <121797615+shahzad-wpbrigade@users.noreply.github.com>
2023-01-19 17:28:32 +0500

    Update conn.php

--- Day3/conn.php
+++ Day3/conn.php
@@ -1,4 +1,4 @@
 <?php
     // We Create the Connection form the Database And "table" is the databasename
-      $con = mysqli_connect("localhost","root","","table") or die("connection was not Successful");
+      $con = mysqli_connect('localhost','root','',table') or die('connection was not Successful');
 ?>
