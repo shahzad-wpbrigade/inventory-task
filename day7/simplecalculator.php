f8a878481468fa2190574384d8b8476d8238d2b8
Shahzad <121797615+shahzad-wpbrigade@users.noreply.github.com>
2023-01-20 17:51:28 +0500

    Add files via upload

---
+++ day7/simplecalculator.php
@@ -0,0 +1,85 @@
+<?php
+if( isset( $_POST['sub'] )){
+
+// Addtion Function
+function Sum() {
+ 
+      $num1 = $_POST['num1'];
+      $num2 = $_POST['num2'];
+      $sub  =  $_POST['sub'];
+
+    if( $sub == '+' ) {
+        $ans = $num1 + $num2;
+         echo "<br>";
+         echo($ans);
+      } 
+}
+
+// Substraction Function
+function Subtraction() {
+ 
+     $num1  = $_POST['num1'];
+     $num2  = $_POST['num2'];
+     $sub   =  $_POST['sub'];
+   if ( $sub == '-') {
+        $ans = $num1 - $num2;
+        echo($ans);
+      } 
+}
+
+// Multipalication Function
+function Multiplication() {
+ 
+     $num1  = $_POST['num1'];
+     $num2  = $_POST['num2'];
+     $sub   = $_POST['sub'];
+
+   if ( $sub == '*' ) {
+         $ans = $num1 * $num2;
+         echo($ans);
+      } 
+}
+
+
+// division Function 
+function Division() {
+ 
+     $num1 = $_POST['num1'];
+     $num2 = $_POST['num2'];
+     $sub  =  $_POST['sub'];
+
+    if ( $sub == '/' ) {
+        $ans = $num1 / $num2;
+        echo($ans);
+      } 
+}
+
+// We calls the Function With Function Names
+Sum($ans);
+Subtraction($ans);
+Multiplication($ans);
+Division($ans);
+
+}// isset function curly brace is end 
+
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="simplecalculator.php" method="post">
+       <input type="number" name="num1"><br>
+
+       <input type="number" name="num2"><br>
+        
+       <input type="submit" name="sub" value="+">
+       <input type="submit" name="sub" value="-">
+       <input type="submit" name="sub" value="*">
+       <input type="submit" name="sub" value="/">
+</form>
+
+
+
+</body>
+</html>
